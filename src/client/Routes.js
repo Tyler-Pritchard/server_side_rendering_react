d64b37a00643d71883485012c12caa7114ae179b
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-08-18 13:47:31 -0700

    dispatch store to fetchUsers

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
-import UsersList from "./components/UsersList";
+import UsersList, {
+  loadData
+} from "./components/UsersList";
 
 export default [{
     path: '/',
@@ -7,6 +9,7 @@ export default [{
     exact: true
   },
   {
+    loadData,
     path: '/users',
     components: UsersList
   }
