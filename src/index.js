397638e02f9ef7d88ed3b26b826512d21b42198d
Tyler Pritchard <pritchard.tyler@gmail.com>
2019-08-26 17:18:52 -0700

    add auth hoc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import "babel-polyfill";
 import express from "express";
-import {
-  matchRoutes
-} from "react-router-config";
+import { matchRoutes } from "react-router-config";
 import proxy from "express-http-proxy";
 import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
@@ -10,21 +8,29 @@ import createStore from "./helpers/createStore";
 
 const app = express();
 
-
-app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
-  proxyReqOptDecorator(opts) {
-    opts.headers['x-forwarded-host'] = 'localhost:3000';
-  }
-}));
+app.use(
+  "/api",
+  proxy("http://react-ssr-api.herokuapp.com", {
+    proxyReqOptDecorator(opts) {
+      opts.headers["x-forwarded-host"] = "localhost:3000";
+    }
+  })
+);
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore(req);
 
-  const promises = matchRoutes(Routes, req.path).map(({
-    route
-  }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(Routes, req.path)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null;
+    })
+    .map(promise => {
+      if (promise) {
+        return new Promise((resolve, reject) => {
+          promise.then(resolve).catch(resolve);
+        });
+      }
+    });
 
   Promise.all(promises).then(() => {
     const context = {};
